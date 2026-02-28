a90633b44a79a8c0a05fd391dcf0bf0053d20bbe
Jahni Morris <jmorris42@fingerlakes.edu>
2026-02-28 14:16:41 +0000

    Fixed spelling correction in line 26.

--- firewall_traffic_analyzer.py
+++ firewall_traffic_analyzer.py
@@ -23,7 +23,7 @@ transfer_size = int(input("Enter the data transfer size in megabytes (MB): "))
 
 
 if (port_number == 22 or port_number == 3389) and transfer_size >= 100:
-    risk = "HIGH RISK: Potential unauthoried remote access detected!"
+    risk = "HIGH RISK: Potential unauthorized remote access detected!"
 elif port_number == 80 and transfer_size > 100:
     risk = "MEDIUM RISK: Large unencrypted data transfer detected."
 elif port_number == 443:
